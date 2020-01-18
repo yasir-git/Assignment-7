35da0374e765342526cd985c7c4f3495556d3d62
yasir-git <59909129+yasir-git@users.noreply.github.com>
2020-01-18 20:17:43 +0000

    Add files via upload

---
+++ lib.rs
@@ -0,0 +1,7 @@
+pub mod project  {
+    pub mod  billion {
+            pub fn trees () {
+            println!("We are in govt 1 billion trees project library ");
+        }
+            }    
+                }
---
+++ main.rs
@@ -0,0 +1,21 @@
+mod province {
+    pub mod kpk {
+        pub fn peshawar () {
+        println!("we are in KPK peshawar");
+     }
+        }
+            }
+ mod lib;
+ use lib::project;
+ 
+
+fn main() {
+
+    crate::province::kpk::peshawar();
+    
+    project::brt::timeline();
+
+    pk_lib::project::billion::trees();
+
+
+}
